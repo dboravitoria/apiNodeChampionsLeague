a0b7909370e8767fe5cdec7ba1290154f8a6c70a
Débora <dboravitoriasilva16@gmail.com>
2025-07-30 21:21:53 -0300

    Adicionando jogadores

--- src/services/playersServices.ts
+++ src/services/playersServices.ts
@@ -1,7 +1,8 @@
+import { findPlayers } from "../repositories/player"
 import { noContent, ok } from "../utils/httpResponse"
 
 export const getPlayerService = async ()=>{
-    const data = {player:""}
+    const data = await findPlayers()
     let response = null
     if(data){
         response = await ok(data)
